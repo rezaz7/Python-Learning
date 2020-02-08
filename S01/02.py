374a9ce21d130756298694a8ef5df95e753e7bc7
Reza <Rezazakeri021@yahoo.com>
2020-02-08 18:30:53 +0330

    Calculator

---
+++ S01/02.py
@@ -0,0 +1,8 @@
+a=input('pls1=')
+a=float(a)
+b=float(input('pls2='))
+if (a!=0) and (b!=0):
+    print(a/b)
+else:
+    print('Invalid')    
+
---
+++ S02/01.py
@@ -0,0 +1,15 @@
+a=float(input('First Number = '))
+b=float(input('Second Number = '))
+c=str(input('Operator = '))
+
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c=='/':
+    print(a/b)
+else:
+    print('Invalid')
+    
