662b9ee84d9be862ca4e37f65b756244f98fd474
Reza <Rezazakeri021@yahoo.com>
2020-02-09 13:41:48 +0330

    Calculator01

--- S02/05.py
+++ S02/05.py
@@ -3,6 +3,7 @@ b=float(input('Second Number = '))
 c=str(input('Operator = '))
 
 if (a!=0) and (b!=0):
+    print('Problem')
     if c=='+':
         print(a+b)
     elif c=='-':
@@ -12,7 +13,7 @@ if (a!=0) and (b!=0):
     elif c=='/':
         print(a/b)
     elif c=='~':
-        print(a**b)
+        print(a**b)    
 else:
     print('Invalid')
     
