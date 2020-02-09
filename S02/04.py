6f894716b151e77fda39207a2410e4dba97f0693
Reza <Rezazakeri021@yahoo.com>
2020-02-09 12:20:09 +0330

    Calculator02

--- S02/04.py
+++ S02/04.py
@@ -2,15 +2,15 @@ a=float(input('First Number = '))
 b=float(input('Second Number = '))
 c=str(input('Operator = '))
 
-
-if c=='+':
-    print(a+b)
-elif c=='-':
-    print(a-b)
-elif c=='*':
-    print(a*b)
-elif c=='/':
-    print(a/b)
+if (a!=0) and (b!=0):
+    if c=='+':
+     print(a+b)
+    elif c=='-':
+     print(a-b)
+    elif c=='*':
+     print(a*b)
+    elif c=='/':
+     print(a/b)
 else:
     print('Invalid')
     
